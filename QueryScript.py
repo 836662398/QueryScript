9fa8394baae595017749d53f9c5e3b8905eb2474
836662398 <46443031+836662398@users.noreply.github.com>
2022-05-20 00:32:57 +0800

    Update QueryScript.py

--- QueryScript.py
+++ QueryScript.py
@@ -1,19 +1,27 @@
+# -*- coding: UTF-8 -*-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.common.by import By
 
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
 url1 = 'http://ehall.seu.edu.cn/gsapp/sys/wddbsqappseu/*default/index.do?t_s=1652969354823&amp_sec_version_=1&gid_=T1ljeGQwY3J6OEhKSWd3RWZHcjQ5b1JmQ3dvRVMxdHd5aTd3ak9BL1lrQUVJYm5MRGwwUTVJaTFwOHFhcmZ6T21KMVZQc2tzbldOY0ZFWVVzV2tmclE9PQ&EMAP_LANG=zh&THEME=indigo#/xsbdjcsq'
 url = "http://ehall.seu.edu.cn/gsapp/sys/wddbsqappseu/modules/xsbdjcsq/lwssjgcx.do"
 
+#设置服务器
+mail_host="smtp.163.com"
+mail_user="***@163.com"
+# 163授权码
+mail_pass="***"
 
 def login(user, pw, browser):
     browser.get(url1)
     browser.implicitly_wait(10)
 
     # 填写用户名密码
-
     username = browser.find_element(By.ID, 'username')
     password = browser.find_element(By.ID, 'password')
     username.clear()
@@ -25,8 +33,30 @@ def login(user, pw, browser):
     login_button = browser.find_element(by=By.CLASS_NAME, value='auth_login_btn')
     login_button.submit()
 
+def mail(msg):
+    sender = '***@163.com'
+    receivers = ['***@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+    message = MIMEText(msg, 'plain', 'utf-8')
+    message['From'] = Header("XR", 'utf-8')
+    message['To'] = Header("XR", 'utf-8')
+
+    subject = '信息更改'
+    message['Subject'] = Header(subject, 'utf-8')
+
+    try:
+        # smtpObj = smtplib.SMTP()
+        smtpObj = smtplib.SMTP_SSL(mail_host, 465)
+        # smtpObj.connect(mail_host, 25)  # 25 为 SMTP 端口号
+        smtpObj.login(mail_user, mail_pass)
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        print("邮件发送成功")
+    except smtplib.SMTPException as e:
+        print(repr(e))
+        print("Error: 无法发送邮件")
+
 if __name__ == "__main__":
-    # 账号
+
     user = ''
     # 密码
     pw = ''
@@ -46,13 +76,35 @@ if __name__ == "__main__":
     browser.implicitly_wait(10)
     content_str = browser.page_source
     l = content_str.split(',')
+    hash_code = ''
+    ZJID = []
+    CJ = []
+    SCHOOL = []
     for ele in l:
         if ele[0:5] == '"ZJID':
             print(ele[7:11])
+            ZJID.append(ele[7:11])
         elif ele[0:4] == '"CJ"':
             print(ele[5:11])
+            CJ.append(ele[5:11])
         elif ele[0:7] == '"SSDWMC':
             print(ele[10:-1] + ':')
+            SCHOOL.append(ele[10:-1])
     browser.quit()
+
+    for zjid in ZJID:
+        hash_code += zjid
+    for cj in CJ:
+        hash_code += cj
+    print(hash_code)
+
+    # 如果信息改变，发送邮件通知用户
+    f = open("hash.txt", 'w+')
+    if hash_code != f.read():
+        print("Content changed!")
+        mail(hash_code)
+        f.write(hash_code)
+
+    f.close()
 
     time.sleep(100)
